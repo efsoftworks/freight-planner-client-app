37b0059bd9aa4e556bb2a90d381af64ca7c357df
= <info@farunurisonmez.com>
2024-03-29 19:26:09 +0300

    feat: #8
    Implemented camera controls using OrbitControls from Three.js examples. The OrbitControls module has been imported, and camera controls have been initialized with it. This enables users to interactively manipulate the camera within the Three.js scene using mouse and touch input.
    
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- src/app/utils/scene/camera.util.ts
+++ src/app/utils/scene/camera.util.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three"
-
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 /**
  * Loads the camera to the given Three.js scene and attaches the renderer to the provided container reference.
  * @param {THREE.Scene} scene - The Three.js scene where the camera will be loaded.
@@ -7,33 +7,37 @@ import * as THREE from "three"
  * @returns {void}
  */
 export function loadCamera(scene: THREE.Scene, containerRef: React.RefObject<HTMLDivElement>): void {
-    // Create a WebGL renderer with antialiasing and alpha transparency.
-    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
     // Define the camera parameters.
     const fov = 60;
     const aspect = window.innerWidth / window.innerHeight;
-    const near = 1.0;   
+    const near = 1.0;
     const far = 10000.0;
 
     // Create a PerspectiveCamera with the defined parameters.
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
     // Set the initial position of the camera.
-    camera.position.set(1000, 400, 800)
+    camera.position.set(0, 100, 500)
 
     // Add fog to the scene for depth perception.
     scene.fog = new THREE.FogExp2(0x89b2eb, 0.000002);
 
 
     if (containerRef.current) {
+        // Create a WebGL renderer with antialiasing and alpha transparency.
+
         // Set renderer properties.
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         renderer.setPixelRatio(window.devicePixelRatio);
         renderer.setSize(window.innerWidth, window.innerHeight);
-       
         // Attach the renderer to the container element.
         containerRef.current.appendChild(renderer.domElement);
 
+        const controls = new OrbitControls(camera, renderer.domElement);
+
+
+        
         const renderScene = () => {
             requestAnimationFrame(renderScene);
             renderer.render(scene, camera);
@@ -41,5 +45,5 @@ export function loadCamera(scene: THREE.Scene, containerRef: React.RefObject<HTM
 
         renderScene();
     }
-   
+
 }
